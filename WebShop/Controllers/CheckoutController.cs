25c8466b7c9260a18242fb8176b53569549a290f
Mario <mario.salai@gmail.com>
2017-09-15 16:53:37 +0200

    Webshop - osnovno

--- WebShop/Controllers/CheckoutController.cs
+++ WebShop/Controllers/CheckoutController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WebShop.Models;
+
+using System.Net.Mail;
+using System.Text;
 
 namespace WebShop.Controllers
 {
@@ -16,8 +20,32 @@ namespace WebShop.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(AddressInformation address)
+        public ActionResult Index(AddressInformation addressd)
         {
+            //SmtpClient client = new SmtpClient();
+            //client.Port = 587;
+            //client.Host = "smtp.gmail.com";
+            //client.EnableSsl = true;
+            //client.Timeout = 10000;
+            //client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            //client.UseDefaultCredentials = false;
+            //client.Credentials = new NetworkCredential("visioinventum@gmail.com", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+            //string bodytext = "Nova narudžba je zaprimljena! Kupac: " + addressd.FirstName + " " + addressd.LastName + Environment.NewLine;
+            //var products = (List<Item>)Session["cart"];
+            //decimal total = 0;
+            //foreach (var x in products)
+            //{
+            //    total += (x.Count * x.Product.Price);
+            //    bodytext += (x.Product.Name + " " + x.Product.Price + " " + x.Count + Environment.NewLine);
+            //}
+
+            //bodytext += Environment.NewLine + "Total sum: " + total;
+            //MailMessage mm = new MailMessage("visioinventum@gmail.com", "mario.salai@gmail.com", "Narudzba", bodytext);
+            //mm.BodyEncoding = UTF8Encoding.UTF8;
+            //mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+            //client.Send(mm);
             return View("OrderCompleted");
         }
     }
--- WebShop/Controllers/HomeController.cs
+++ WebShop/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebShop.Models;
+using PagedList;
+using PagedList.Mvc;
 
 namespace WebShop.Controllers
 {
@@ -35,10 +37,38 @@ namespace WebShop.Controllers
             return PartialView(categories);
         }
 
-        public ActionResult Products(int cigla)
+        public ActionResult Products(int cigla, string SearchText, int? page, string sortOrder)
         {
             ShopContext db = new ShopContext();
             var products = db.Products.Where(p => p.CategoryId == cigla).ToList();
+
+            //#region search
+            //if (string.IsNullOrEmpty(SearchText) == false)
+            //{
+            //    products = products.Where(x => x.Name.Contains(SearchText)).ToList();
+            //}
+            //ViewBag.search = SearchText;
+            //#endregion
+
+            //#region sort
+            ////ViewBag.sort = sortOrder == "Price" ? "PriceDesc" : "Price";
+            //ViewBag.sort = sortOrder;
+            //if (ViewBag.sort == null) ViewBag.sort = "Price";
+
+            //switch (sortOrder)
+            //{
+            //    case "Price":
+            //        products = products.OrderBy(x => x.Price).ToList();
+            //        break;
+            //    case "PriceDesc":
+            //        products = products.OrderByDescending(x => x.Price).ToList();
+            //        break;
+            //    default:
+            //        products = products.OrderBy(x => x.Price).ToList();
+            //        break;
+            //}
+            //#endregion
+            //return View(products.ToPagedList(page ?? 1, 6));
             return View(products);
         }
 
